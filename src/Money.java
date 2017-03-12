296f857d0fb9259b1fec558ed32d94f3bb819140
Xsolute <xsolute01@gmail.com>
2017-03-12 09:05:30 +0900

    완성판 보완
    자판기 프로젝트 소스 보완

--- src/Money.java
+++ src/Money.java
@@ -9,11 +9,11 @@ public class Money {
         System.out.println("입금하실 액수를 입력하십시오 : ");
         bill += scanner.nextInt();
         System.out.print(bill);
-        System.out.println("원 입금되었습니다.");
+        System.out.println("원 입금되었습니다. 현재 잔돈 : " + bill + "원");
     }
     public void ReturnMoney() {
         System.out.print(bill);
-        System.out.print("원 환불되었습니다.");
+        System.out.println("원 환불되었습니다.");
         bill = 0;
     }
 }
